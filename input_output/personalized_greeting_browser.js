c7af293f08dc4965bd34853d154bcb7ae4a892c7
f10at1n9-p01nt <kwyaiko@gmail.com>
2021-10-24 22:37:19 -0400

    Finishes input/output (Chapter 3) exercises.

---
+++ input_output/personalized_greeting_browser.js
@@ -0,0 +1,2 @@
+let name = prompt("What's your name?\n");
+console.log(`Good Morning, ${name}`);
--- variables/age.js
+++ variables/age.js
@@ -1,4 +1,5 @@
-let age = 39;
+let rlSync = require('readline-sync');
+let age = Number(rlSync.question("How old are you?\n"));
 
 console.log(`You are ${age} years old.`);
 console.log(`In 10 years, you will be ${age + 10} years old.`);
